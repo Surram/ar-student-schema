1d4097dae8bd6e5769a5ab7114d2f51c8871a1f5
Team1 <student@devbootcamp.com>
2013-10-23 17:59:35 -0500

    added validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,34 +3,44 @@ require_relative '../../db/config'
 class Student < ActiveRecord::Base
 # implement your Student model here
   
-  attr_accessor :first_name, :last_name, :gender, :birthday
-  attr_writer :age
+  # attr_accessor :first_name, :last_name, :gender, :birthday, :phone, :email
+  # attr_writer :age
 
-  def initialize #(params = {})
-    # @first_name = params[:first_name]
-    # @last_name = params[:last_name]
-    # @gender = params[:gender]
-    # @birthday = params[:birthday]
-    # @age = params[:age]
-  end
+  
+  validates :email, :format => {:with => /\A.+@.+[.].{2,}\z/, :message => "email address must be validly formatted" }#, :uniqueness => true
+  validates :email, :uniqueness => true 
+  validates :age, :numericality => {only_integer: true, greater_than_or_equal_to: 5}
+  validates :phone, :format => { :with => /(\d{3}).*(\d{3}).*(\d{4})/, :message => "phone number must be valid"}
 
-  def assign_attributes(params = {})
-    @first_name = params[:first_name]
-    @last_name = params[:last_name]
-    @gender = params[:gender]
-    @birthday = params[:birthday]
-    @email = params[:email]
-    @phone = params[:phone]
-    @age = params[:age]
-  end
+  # def initialize #(params = {})
+  #   # @first_name = params[:first_name]
+  #   # @last_name = params[:last_name]
+  #   # @gender = params[:gender]
+  #   # @birthday = params[:birthday]
+  #   # @age = params[:age]
+  # end
 
-  def name 
-    first_name + " " + last_name
-  end
+  # def assign_attributes(params = {})
+  #   @first_name = params[:first_name]
+  #   @last_name = params[:last_name]
+  #   @gender = params[:gender]
+  #   @birthday = params[:birthday]
+  #   @email = params[:email]
+  #   @phone = params[:phone]
+  #   @age = params[:age]
+  # end
+
+  # def name 
+  #   first_name + " " + last_name
+  # end
 
   def age
-    DateTime.now.year - @birthday.year
+    DateTime.now.year - birthday.year
   end
+
+  # def valid?
+  #   if
+  # end
 end
 
-p DateTime.now.year - Date.parse('1988-04-24').year
+# p DateTime.now.year - Date.parse('1988-04-24').year
